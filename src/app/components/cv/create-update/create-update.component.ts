ac81eb85b1f1ab8d759e590bade3de47a84deb49
emilia-eberhart <emilia.eberhart@hftm.ch>
2024-12-16 19:48:32 +0100

    Vorbereitungsauftrag
    Vorbereitungsauftrag - Button Edit & New anpassen

--- src/app/components/cv/create-update/create-update.component.ts
+++ src/app/components/cv/create-update/create-update.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { CVService } from '../../../services/cv.service';
+import { map, mergeMap, of } from 'rxjs';
+import { CV } from '../../../models/cv';
 
 @Component({
   selector: 'app-create-update',
   templateUrl: './create-update.component.html',
   styleUrl: './create-update.component.scss'
 })
-export class CreateUpdateComponent {
+export class CreateUpdateComponent implements OnInit{
 
-}
+    formGroup?: FormGroup;
+
+    constructor(
+      private route: ActivatedRoute,
+      private cvService: CVService,
+    ){}
+
+    ngOnInit(): void {
+      this.route.paramMap.pipe(
+        map(params => params.get('id')),
+        mergeMap(id => {
+          if (!id){
+            return of(null);
+          } else {
+            return this.cvService.getCVById(id!)  
+          } 
+        })
+      ).subscribe(cv => this.initForm(cv))
+    }
+  
+    initForm(cv: CV| null): void{
+      this.formGroup = new FormGroup({
+        employer: new FormControl(cv?.employer ?? null, [Validators.required]),
+        start: new FormControl(cv?.start ?? null, [Validators.required]),
+        end: new FormControl(cv?.end ?? null, []),
+      });
+      if (cv?.id) {
+        this.formGroup.addControl('id', new FormControl(cv?.id ?? null, []))
+      }
+    } 
+  
+    saveCV(): void {
+      if (this.formGroup?.valid) {
+        let observable = null;
+        if (this.formGroup.value.id) {
+          observable = this.cvService.updateCV(this.formGroup.value.id, this.formGroup.value);
+        } else {
+          observable = this.cvService.createCV(this.formGroup.value);
+        }
+        observable.subscribe(() => history.back());
+      } else {
+        alert('CV is invalid');
+      }
+    }
+  }
+  
+
